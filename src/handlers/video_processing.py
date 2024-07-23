2a447cd6bc01b30796a2582c4b307b62de48e423
fernanda-meza-wizeline <luisa.meza@wizeline.com>
2024-07-22 21:09:03 -0700

    Sync with main

--- src/handlers/video_processing.py
+++ src/handlers/video_processing.py
@@ -2,6 +2,15 @@ import os
 import urllib.request 
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
+# videoName : str
+#       Path to the file from which the subclip will be extracted.
+# startTimestamp : float
+#       Moment of the input clip that marks the start of the produced subclip.
+# endTimestamp : float
+#       Moment of the input clip that marks the end of the produced subclip.
+# clipName : str, optional
+#       Path to the output file. Defaults to
+#       ``<inputfile_name>SUB<start_time>_<end_time><ext>``.
 def create_clip_from_video(videoName,startTimestamp,endTimestamp, clipName)
 #placeholder, using presigned url
 videoPath=os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'clips'))
