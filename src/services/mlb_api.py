5330d2806485723c5e9c7a79d603447b5c42b586
fabianpg95 <fabian.portillo@fox.com>
2024-07-23 10:41:56 -0600

    Add gamebyplay endpoint functionality to retrieve its information

--- src/services/mlb_api.py
+++ src/services/mlb_api.py
@@ -11,82 +11,17 @@ class MLBAPI:
         # Init caché for errores 404
         self.cache = {}
 
-    def get_players(self, person_ids):
-        url = f'{self.BASE_URL}/players'
-        if isinstance(person_ids, list) and all(isinstance(pid, int) for pid in person_ids):
-            person_ids_str = ','.join(str(pid) for pid in person_ids)
-            params = {'personIds': person_ids_str}
-            print(f"Params: {params}") 
-        else:
-            raise ValueError("person_ids debe ser una lista de enteros")
-        
+    def get_games_playbyplay(self, game_pk):
+        if not isinstance(game_pk, int):
+            raise ValueError("game_pk must be int")
+
+        url = f'{self.BASE_URL}/games-playbyplay'
+        params = {'gamePk': game_pk}
+        print(f"Params: {params}")
+
         response = requests.get(url, headers=self.headers, params=params)
         print(f"Response status code: {response.status_code}")
         print(f"Response content: {response.text}")
-        return self._check_response(response)
-
-    def get_free_agents(self, season_id):
-        url = f'{self.BASE_URL}/free-agents'
-        params = {'seasonId': str(int(season_id))}
-        response = requests.get(url, headers=self.headers, params=params)
-        return self._check_response(response)
-
-    def get_schedule(self, date):
-        url = f'{self.BASE_URL}/schedule'
-        params = {'date': date}
-        response = requests.get(url, headers=self.headers, params=params)
-        return self._check_response(response)
-
-    def get_teams(self, team_id):
-        url = f'{self.BASE_URL}/teams'
-        params = {'teamId': str(int(team_id))}
-        response = requests.get(url, headers=self.headers, params=params)
-        return self._check_response(response)
-
-    def get_teams_history(self, team_ids):
-        url = f'{self.BASE_URL}/teams/history'
-        if isinstance(team_ids, list) and all(isinstance(tid, int) for tid in team_ids):
-            team_ids_str = ','.join(str(tid) for tid in team_ids)
-            params = {'teamIds': team_ids_str}
-            print(f"Params: {params}")
-        else:
-            raise ValueError("team_ids debe ser una lista de enteros")
-    
-        response = requests.get(url, headers=self.headers, params=params)
-        print(f"Response status code: {response.status_code}")
-        print(f"Response content: {response.text}")
-        return self._check_response(response)
-
-    def get_teams_coaches(self, team_ids):
-        url = f'{self.BASE_URL}/teams-coaches'
-        if isinstance(team_ids, list) and all(isinstance(tid, int) for tid in team_ids):
-            team_ids = ','.join(str(tid) for tid in team_ids)
-        params = {'teamIds': team_ids}
-        response = requests.get(url, headers=self.headers, params=params)
-        return self._check_response(response)
-
-    def get_teams_personnel(self, team_ids):
-        url = f'{self.BASE_URL}/teams-personnel'
-        if isinstance(team_ids, list) and all(isinstance(tid, int) for tid in team_ids):
-            team_ids = ','.join(str(tid) for tid in team_ids)
-        params = {'teamIds': team_ids}
-        response = requests.get(url, headers=self.headers, params=params)
-        return self._check_response(response)
-
-    def get_teams_affiliates(self, team_ids):
-        url = f'{self.BASE_URL}/teams-affiliates'
-        if isinstance(team_ids, list) and all(isinstance(tid, int) for tid in team_ids):
-            team_ids = ','.join(str(tid) for tid in team_ids)
-        params = {'teamIds': team_ids}
-        response = requests.get(url, headers=self.headers, params=params)
-        return self._check_response(response)
-
-    def get_teams_roster(self, team_ids):
-        url = f'{self.BASE_URL}/teams-roster'
-        if isinstance(team_ids, list) and all(isinstance(tid, int) for tid in team_ids):
-            team_ids = ','.join(str(tid) for tid in team_ids)
-        params = {'teamIds': team_ids}
-        response = requests.get(url, headers=self.headers, params=params)
         return self._check_response(response)
 
     def _check_response(self, response):
