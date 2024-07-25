0f98081efdbd76de57ecbc7c49b3874771a8c63e
NardaBallesterosW <98483064+NardaBallesterosW@users.noreply.github.com>
2024-07-25 12:18:54 -0600

    fix fixing sh file

--- src/handlers/upload_video.sh
+++ src/handlers/upload_video.sh
@@ -1,6 +1,6 @@
-python youtube_api.py --file=“/Users/narda.ballesteros/Desktop/video.mp4”
-                       --title=“Sport_video”
-                       --description=“home run”
-                       --keywords=“sport,homerun”
-                       --category=“17”
-                       --privacyStatus=“private”
+python youtube_api.py  --file="/Users/narda.ballesteros/Desktop/junio.mp4"
+                       --title="Sport_video"
+                       --description="home run"
+                       --keywords="sport,homerun"
+                       --category="17"
+                       --privacyStatus="private"
