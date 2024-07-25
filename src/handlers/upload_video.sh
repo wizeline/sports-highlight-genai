0461c16bb803e09240f635142ab62d327b998b9f
NardaBallesterosW <98483064+NardaBallesterosW@users.noreply.github.com>
2024-07-25 13:19:44 -0600

    Fix tittle and description

--- src/handlers/upload_video.sh
+++ src/handlers/upload_video.sh
@@ -1,6 +1,6 @@
-python youtube_api.py  --file="/Users/narda.ballesteros/Desktop/junio.mp4"
-                       --title="Sport_video"
-                       --description="home run"
-                       --keywords="sport,homerun"
-                       --category="17"
+python youtube_api.py  --file="/Users/narda.ballesteros/Desktop/junios.mp4" \
+                       --title="Sport_video" \
+                       --description="home run" \
+                       --keywords="sport,homerun" \
+                       --category="17" \
                        --privacyStatus="private"
