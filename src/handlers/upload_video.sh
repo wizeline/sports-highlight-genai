de6e63248b4f0fa7312c2302b0eb2bd096130d80
NardaBallesterosW <98483064+NardaBallesterosW@users.noreply.github.com>
2024-07-25 11:59:25 -0600

    add sh and secrets

---
+++ src/handlers/upload_video.sh
@@ -0,0 +1,6 @@
+python youtube_api.py --file=“/Users/narda.ballesteros/Desktop/video.mp4”
+                       --title=“Sport_video”
+                       --description=“home run”
+                       --keywords=“sport,homerun”
+                       --category=“17”
+                       --privacyStatus=“private”
